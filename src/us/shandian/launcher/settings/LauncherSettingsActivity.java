6c19884ab7091f64d6323b86b6c885d3e54358be
PeterCxy <xqsx43cxy@126.com>
2014-01-30 13:12:23 +0800

    LauncherSettingsActivity: restart

--- src/us/shandian/launcher/settings/LauncherSettingsActivity.java
+++ src/us/shandian/launcher/settings/LauncherSettingsActivity.java
@@ -1,15 +1,36 @@
 package us.shandian.launcher.settings;
 
 import android.os.Bundle;
+import android.content.Context;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
 
 import us.shandian.launcher.R;
 
-public class LauncherSettingsActivity extends PreferenceActivity
+public class LauncherSettingsActivity extends PreferenceActivity implements OnPreferenceClickListener
 {
+    private Preference mRestart;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
+        
+        // Initialize the preferences
+        mRestart = findPreference(SettingsProvider.KEY_SETTINGS_RESTART);
+        mRestart.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference)
+    {
+        if (preference == mRestart) {
+            // I am Launcher, so I will restart after killing
+            System.exit(0);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
---
+++ src/us/shandian/launcher/settings/SettingsProvider.java
@@ -0,0 +1,6 @@
+package us.shandian.launcher.settings;
+
+public class SettingsProvider
+{
+    public static String KEY_SETTINGS_RESTART = "settings_restart";
+}
